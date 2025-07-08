bb8bd7856ffdd11a16549a36ec030e85992c68cd
Yusuke Inoue <inouey1008@gmail.com>
2025-07-08 22:38:12 +0900

    eslint.config.js, package.json の記述見直し

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
