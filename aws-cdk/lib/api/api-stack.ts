37e9190ada947c379eed5169d16a044337bdcb5c
Yusuke Inoue <inouey1008@gmail.com>
2025-07-13 00:31:47 +0900

    Contact Table の定義変更

--- aws-cdk/lib/api/api-stack.ts
+++ aws-cdk/lib/api/api-stack.ts
@@ -127,6 +127,7 @@ export class ApiStack extends cdk.Stack {
     const table = new dynamodb.Table(this, "ContactTable", {
       tableName: `fastify-api-templete-contact-table`,
       partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "timestamp", type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
     table.grantReadWriteData(lambdaFunction);
